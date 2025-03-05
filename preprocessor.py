523181be8c4b044a7c1c8f51e45df2cdb9fd30d0
Samarth Bhamare <77913972+Samarth0211@users.noreply.github.com>
2025-03-05 13:06:52 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -2,21 +2,32 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    # Fix date pattern (removed extra `-` and added AM/PM handling)
+    pattern = r'\d{1,2}/\d{1,2}/\d{2},\s\d{1,2}:\d{2}\s?[apAP][mM]'
+
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
+
+    # Remove unwanted Unicode spaces (zero-width spaces or special spaces)
+    dates = [re.sub(r'\s+', ' ', date).strip() for date in dates]
+
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M - ')
+
+    # Fix datetime parsing (24-hour format with two-digit year `%y`)
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %I:%M %p')
+
     df.rename(columns={'message_date': 'date'}, inplace=True)
+
     users = []
     messages = []
+
     for message in df['user_message']:
-        entry = re.split('([\w\W]+?):\s', message)
-        if entry[1:]:
+        entry = re.split(r'([\w\W]+?):\s', message, maxsplit=1)
+        if len(entry) > 2:
             users.append(entry[1])
             messages.append(entry[2])
         else:
-            users.append('chat_notification')
+            users.append("Unknown")
             messages.append(entry[0])
 
     df['user'] = users
@@ -32,14 +43,16 @@ def preprocess(data):
     df['minutes'] = df['date'].dt.minute
     df = df[df['user'] != 'chat_notification']
 
+    # Fix Period Calculation
     period = []
-    for hour in df[['day_name', 'hour']]['hour']:
+    for hour in df['hour']:
         if hour == 23:
-            period.append(str(hour) + '-' + str('00'))
+            period.append("23-00")
         elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
+            period.append("00-01")
         else:
-            period.append(str(hour) + "-" + str(hour + 1))
+            period.append(f"{hour}-{hour+1}")
 
     df['period'] = period
+
     return df
